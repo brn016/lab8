eba38097f2f266f5f32dd60dfba7be815a8a76d5
Brandon Nguyen <brn016@ucsd.edu>
2018-03-01 10:41:25 -0800

    finished as far as i could get

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,17 @@
 function initMap() {
 	// add your code here
+	var ucsd_ltlng = {lat:32.879039, lng:-117.236198};
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: ucsd_ltlng,
+		zoom: 15
+	});
+
+	var marker = new google.maps.Marker({
+		position: ucsd_ltlng,
+		map: map,
+		title: "UCSD"
+	});
+
+	//console.log("map working")
 }
